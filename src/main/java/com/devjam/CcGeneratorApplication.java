8ddb14d0921fa1e486ae48965ed9febdbefb1387
Neeraj <suthar.nrj@gmail.com>
2024-06-22 12:34:25 +0530

    Init

---
+++ src/main/java/com/devjam/CcGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.devjam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devjam/CcGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
